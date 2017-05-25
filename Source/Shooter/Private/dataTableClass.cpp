317f343510603a60ac131ec9e316063623738857
gitvalt <valtteri.seuranen@gmail.com>
2017-05-25 16:48:32 +0300

    tiedon tallentamista

---
+++ Source/Shooter/Private/dataTableClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Shooter.h"
+#include "dataTableClass.h"
+
+dataTableClass::dataTableClass()
+{
+}
+
+dataTableClass::~dataTableClass()
+{
+}
