2de0e9006fc91a1c7c2a2989eaffaeed6afbce4d
K5680 <k5680@student.jamk.fi>
2017-05-26 19:26:45 +0300

    Add files via upload

---
+++ Intermediate/Build/Win64/UE4Editor/Development/Shooter/PCH.Shooter.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/Vesada/Downloads/gameproject2017-master(5)/gameproject2017-master/Intermediate/Build/Win64/UE4Editor/Development/Shooter/PCH.Shooter.h"
---
+++ Source/Shooter/Public/dataTableClass.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class SHOOTER_API dataTableClass
+{
+public:
+	dataTableClass();
+	~dataTableClass();
+};
